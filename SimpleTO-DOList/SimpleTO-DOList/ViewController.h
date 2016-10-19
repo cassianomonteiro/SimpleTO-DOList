6442971b40218a2def9d69795ff67b3faef33347
Cassiano Monteiro <cassianomonteiro@gmail.com>
2016-10-19 20:13:30 -0200

    Initial project setup.

---
+++ SimpleTO-DOList/SimpleTO-DOList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTO-DOList
+//
+//  Created by Cassiano Monteiro on 19/10/16.
+//  Copyright © 2016 Cassiano Monteiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleTO-DOList/SimpleTO-DOList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTO-DOList
+//
+//  Created by Cassiano Monteiro on 19/10/16.
+//  Copyright © 2016 Cassiano Monteiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
